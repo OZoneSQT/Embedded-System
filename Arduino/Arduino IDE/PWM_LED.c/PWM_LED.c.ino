5c58ef470769f4ada3e869db16bd091bfcd58c12
Christopher McArthur <prince.chrismc@gmail.com>
2017-09-27 14:35:19 -0400

    trying bare metal from Arduino IDE

---
+++ Arduino/Arduino IDE/PWM_LED.c/PWM_LED.c.ino
@@ -0,0 +1,36 @@
+int main(void)
+{
+   // initialize the serial communication:
+   Serial.begin(115200);
+   int serial_max_write = Serial.availableForWrite();
+
+   /* set pin D5 for output internal LED*/
+   DDRB |= (OUTPUT << PB1);
+
+   TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);  //clear on compare match, continue count to OxFF
+   TCCR0B |= (1 << CS02) | (1 << CS00);// prescale counter by 1024
+   TCNT0 = 0;
+   DDRD |= (255);
+   OCR0A = 255;  //initial compare match value
+
+   //Serial.println("Launching...");
+   byte brightness;
+
+   while (1)
+   {
+      if (Serial.available())
+      {
+         // read the most recent byte (which will be from 0 to 255):
+         brightness = Serial.read();
+
+         // print the value
+         //Serial.print("I received: ");
+         //Serial.println(brightness, DEC);
+
+         OCR0A = brightness;
+      }
+   }
+
+   return 0;
+}
+
