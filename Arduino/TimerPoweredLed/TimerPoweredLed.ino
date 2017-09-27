075e8d9007fe678455a6b84b2298f7918c7e5905
Christopher McArtur <prince.chrismc@gmail.com>
2017-09-27 11:13:07 -0400

    LED brightness controlled by serial input

--- Arduino/TimerPoweredLed/TimerPoweredLed.ino
+++ Arduino/TimerPoweredLed/TimerPoweredLed.ino
@@ -1,37 +1,53 @@
 /*
-Blink
+Dimmer
 
-Turns an LED on for one second, then off for one second, repeatedly.
+Demonstrates sending data from the computer to the Arduino board, in this case
+to control the brightness of an LED. The data is sent in individual bytes,
+each of which ranges from 0 to 255. Arduino reads these bytes and uses them to
+set the brightness of the LED.
 
-Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-the correct LED pin independent of which board is used.
-If you want to know what pin the on-board LED is connected to on your Arduino
-model, check the Technical Specs of your board at:
-https://www.arduino.cc/en/Main/Products
+The circuit:
+- LED attached from digital pin 9 to ground.
+- Serial connection to Processing, Max/MSP, or another serial application
 
-modified 8 May 2014
-by Scott Fitzgerald
-modified 2 Sep 2016
-by Arturo Guadalupi
-modified 8 Sep 2016
-by Colby Newman
+created 2006
+by David A. Mellis
+modified 30 Aug 2011
+by Tom Igoe and Scott Fitzgerald
 
 This example code is in the public domain.
 
-http://www.arduino.cc/en/Tutorial/Blink
+http://www.arduino.cc/en/Tutorial/Dimmer
 */
 
-// the setup function runs once when you press reset or power the board
+/*
+   THIS CODE HAS BEEN MODIFIED TO BETTER SUIT MY TEST CASE
+   Chris Mc 27/09/17
+*/
+
+const int ledPin = 9;      // the pin that the LED is attached to
+
 void setup() {
-	// initialize digital pin LED_BUILTIN as an output.
-	pinMode(LED_BUILTIN, OUTPUT);
+   // initialize the serial communication:
+   Serial.begin(115200);
+   // initialize the ledPin as an output:
+   pinMode(ledPin, OUTPUT);
+   analogWrite(ledPin, 255);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(1000);                       // wait for a second
-	digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-	delay(1000);                       // wait for a second
+   byte brightness;
+
+   // check if data has been sent from the computer:
+   if (Serial.available()) {
+      // read the most recent byte (which will be from 0 to 255):
+      brightness = Serial.read();
+
+      // print the value
+      Serial.print("I received: ");
+      Serial.println(brightness, DEC);
+
+      // set the brightness of the LED:
+      analogWrite(ledPin, brightness);
+   }
 }
--- Arduino/TimerPoweredLed/main.cpp
+++ Arduino/TimerPoweredLed/main.cpp
@@ -2,22 +2,29 @@
 // 
 // 
 
-#include "main.h"
-#include <util/delay.h>
-#include <HardwareSerial.h>
-
-int main(void)
-{
-	Serial.begin(115200);
-
-	/* set pin D5 for output internal LED*/
-	DDRB |= (OUTPUT << PB5);
-
-	while (1)
-	{
-		_delay_ms(100);
-		Serial.println("tick");
-	}
-
-	return 0;
-}
+//#include "main.h"
+//#include <util/delay.h>
+//#include <HardwareSerial.h>
+//
+//int main(void)
+//{
+//   Serial.begin(9600);
+//
+//   /* set pin D5 for output internal LED*/
+//   DDRB |= (OUTPUT << PB5);
+//
+//   char* buf = new char[256];
+//   Serial.println("input text");
+//      Serial.readBytes(buf, 256 );
+//
+//      Serial.println(buf);
+//   delete[] buf;
+//   return 0;
+//
+//   while (1)
+//   {
+//      _delay_ms(1000);
+//      Serial.println("tick");
+//   }
+//
+//}
