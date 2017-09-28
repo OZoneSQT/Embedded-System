d19d53b7b79800bc13b542dc6de5f4ff721149b4
Christopher McArthur <prince.chrismc@gmail.com>
2017-09-27 20:36:48 -0400

    commented out code and removed extra include

--- Arduino/TimerPoweredLed/TimerPoweredLed.ino
+++ Arduino/TimerPoweredLed/TimerPoweredLed.ino
@@ -1,40 +1,38 @@
+////
+//// Christopher McArthur 400042
+////
 //
-// Christopher McArthur 400042
+//byte brightness;
 //
-
-#include "Arduino.h"
-byte brightness;
-
-
-void setup() {
-   // initialize the serial communication:
-   Serial.begin(115200);
-   int serial_max_write = Serial.availableForWrite();
-
-   TCCR2A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);  //clear on compare match, continue count to OxFF
-   TCCR2B |= (1 << CS02) | (1 << CS00); // prescale counter by 1024
-   DDRB |= (255); // turn all of PB's to output
-   OCR2A = 255;  //initial compare match value - max brightness
-
-   Serial.println("Launching...");
-   Serial.flush();
-}
-
-void loop()
-{
-   if (Serial.available() > 0)
-   {
-      Serial.println("now reading next char...");
-      Serial.flush();
-
-      // read the most recent byte (which will be from 0 to 255):
-      brightness = Serial.read();
-
-      // print the value
-      Serial.print("I received: ");
-      Serial.println(brightness, DEC);
-      Serial.flush();
-
-      OCR2A = brightness;
-   }
-}
+//void setup() {
+//   // initialize the serial communication:
+//   Serial.begin(115200);
+//   int serial_max_write = Serial.availableForWrite();
+//
+//   TCCR2A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);  //clear on compare match, continue count to OxFF
+//   TCCR2B |= (1 << CS02) | (1 << CS00); // prescale counter by 1024
+//   DDRB |= (255); // turn all of PB's to output
+//   OCR2A = 255;  //initial compare match value - max brightness
+//
+//   Serial.println("Launching...");
+//   Serial.flush();
+//}
+//
+//void loop()
+//{
+//   if (Serial.available() > 0)
+//   {
+//      Serial.println("now reading next char...");
+//      Serial.flush();
+//
+//      // read the most recent byte (which will be from 0 to 255):
+//      brightness = Serial.read();
+//
+//      // print the value
+//      Serial.print("I received: ");
+//      Serial.println(brightness, DEC);
+//      Serial.flush();
+//
+//      OCR2A = brightness;
+//   }
+//}
