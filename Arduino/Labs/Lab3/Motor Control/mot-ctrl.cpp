5aebbf8534f821cd409568359b21d84a2125d924
Christopher McArthur <prince.chrismc@gmail.com>
2017-10-14 13:57:57 -0400

    basic interrupt code

--- Arduino/Labs/Lab3/Motor Control/mot-ctrl.cpp
+++ Arduino/Labs/Lab3/Motor Control/mot-ctrl.cpp
@@ -4,9 +4,23 @@
 
 #include "main.h"
 
+ISR(INT0_vect)
+{
+      // Disable interrupts in case of another interrupt
+      cli();
+      // My code goes here
+
+      // Re-enable interrupts
+      sei();
+}
+
+
 int main(void)
 {
+   EICRA |= (1 << ISC01) | (1 << ISC00);
+   EIMSK |= (1 << INT0);
 
+   sei();
    while (1)
    {
 
