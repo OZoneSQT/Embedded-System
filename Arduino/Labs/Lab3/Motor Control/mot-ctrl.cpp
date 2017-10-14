80287ce000e24d79f7108d3cea6dfb600e27c496
Christopher McArthur <prince.chrismc@gmail.com>
2017-10-14 17:47:22 -0400

    completed rcv code need to figure out how it works

--- Arduino/Labs/Lab3/Motor Control/mot-ctrl.cpp
+++ Arduino/Labs/Lab3/Motor Control/mot-ctrl.cpp
@@ -6,24 +6,30 @@
 
 ISR(INT0_vect)
 {
-      // Disable interrupts in case of another interrupt
-      cli();
-      // My code goes here
+      cli();                                    // Disable interrupts in case of another interrupt
 
-      // Re-enable interrupts
-      sei();
+      Serial.println("Triggered...");
+      PORTC |= (HIGH << PC3);                   // activate LED
+      _delay_ms(500);
+
+      sei();                                    // Re-enable interrupts
 }
 
 
 int main(void)
 {
-   EICRA |= (1 << ISC01) | (1 << ISC00);
-   EIMSK |= (1 << INT0);
+   EICRA |= (1 << ISC01) | (1 << ISC00);         // trigger on rising edge
+   EIMSK |= (1 << INT0);                        // active external pin int0
 
-   sei();
+   DDRC |= (OUTPUT << PC3);                     // listen for signal
+
+   Serial.begin(115200);
+
+   sei();                                       // enable interrupts
    while (1)
    {
-
+      PORTC &= (LOW << PC3);                    // turn off LED
+      _delay_ms(50);
    }
 
    return 0;
