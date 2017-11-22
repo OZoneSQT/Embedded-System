56f915da65d5a4e29613793017780543b703c82f
Chris Mc <prince.chrismc@gmail.com>
2017-11-22 12:32:30 -0500

    moving docs/Template to Docs/Template

--- Arduino/docs/Template/main.h
+++ Arduino/docs/Template/main.h
@@ -1,14 +1,13 @@
 // main.h
-
+ 
 #ifndef _MAIN_h
 #define _MAIN_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
+   #include "Arduino.h"
 #else
-	#include "WProgram.h"
+   #include "WProgram.h"
 #endif
 
-
 #endif
-
+ 
