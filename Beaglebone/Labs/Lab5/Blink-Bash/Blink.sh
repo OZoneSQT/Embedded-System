43f3e6dd94dabd0b52e82488c2cd24c4250b9e19
Chris Mc <prince.chrismc@gmail.com>
2017-11-24 23:41:15 -0500

    switched to user timer for LED control

--- Beaglebone/Labs/Lab5/Blink-Bash/Blink.sh
+++ Beaglebone/Labs/Lab5/Blink-Bash/Blink.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 # https://groups.google.com/d/msg/beagleboard/RQ_FTbP7rHM/PtjW1popct0J
-# attempt to control from cli
-sudo sh -c "echo none > /sys/class/leds/beaglebone\:green\:usr0/trigger"
+# attempt to control from cli -- default is heartbeat
+sudo sh -c "echo none > /sys/class/leds/beaglebone\:green\:usr0/trigger" # attempt 1 (following tutorial)
+
+# trying user specified timer values
+sudo sh -c "echo timer > /sys/class/leds/beaglebone\:green\:usr0/trigger"
+sudo sh -c "echo 100 > /sys/class/leds/beaglebone\:green\:usr0/delay_on"
+sudo sh -c "echo 100 > /sys/class/leds/beaglebone\:green\:usr0/delay_off"
